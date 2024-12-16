fce836c86c08854addbf2f30734001544d87621c
Ayoub Marghad <mr.ayoubmarghad@gmail.com>
2024-12-16 17:09:25 +0100

    Add technical microservices

---
+++ discovery/src/test/java/marghad/ayoub/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package marghad.ayoub.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
